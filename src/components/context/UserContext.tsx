98ec4ef728cc6452bd2df5e6258674cc61d63857
olwpad <olwpad@gmail.com>
2023-11-19 19:55:44 -0500

    Updates

--- src/components/context/UserContext.tsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext } from 'react';
-import { FormState,FormAction } from '../useReducer';
-
-interface UserContextProps {
-  state: FormState;
-  dispatch: React.Dispatch<FormAction>;
-}
-
-export const UserContext = createContext<UserContextProps>({} as UserContextProps);
