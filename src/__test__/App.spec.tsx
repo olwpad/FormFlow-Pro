31ec3ed9cea0528642ece89b4920f5a22d3ca90c
olwpad <olwpad@gmail.com>
2023-12-08 20:20:45 -0500

    Se agregaron pruebas

---
+++ src/__test__/App.spec.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { UserProvider } from '../components/context/userProvider'; // Asegúrate de que sea el nombre correcto del archivo
+import Step1 from '../components/Step1';
+
+describe('Step1 component', () => {
+  it('Renderiza el componente correctamente', () => {
+    render(
+      <MemoryRouter>
+        <UserProvider>
+          <Step1 />
+        </UserProvider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Hello My friend')).toBeTruthy();
+    expect(screen.getByLabelText('FirstName')).toBeTruthy();
+    expect(screen.getByLabelText('Last name')).toBeTruthy();
+    expect(screen.getByText('Next')).toBeTruthy();
+  });
+
+  it(' debe hacer match con el snapshot', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <UserProvider>
+          <Step1 />
+        </UserProvider>
+      </MemoryRouter>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
